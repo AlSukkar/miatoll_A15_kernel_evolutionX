bc6395e4b0efa6f36dd5a526efc26b33e1c92c5e
Abdulrahman Sukkar <aboodsukkar24@live.com>
2025-09-25 04:28:45 +0300

    Create anykernel.sh

---
+++ templates/anykernel.sh
@@ -0,0 +1,40 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=Evolution-X Kernel with KernelSU Next + SUSFS
+do.devicecheck=1
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=miatoll
+device.name2=curtana
+device.name3=excalibur
+device.name4=gram
+device.name5=joyeuse
+supported.versions=11-14
+supported.patchlevels=
+'; } # end properties
+
+# shell variables
+block=boot;
+is_slot_device=1;
+ramdisk_compression=auto;
+patch_vbmeta_flag=auto;
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+
+## AnyKernel boot install
+dump_boot;
+write_boot;
+## end boot install
