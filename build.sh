7b784d1241b5681d55800ad5b4e2f0f315de0b60
Abdulrahman Sukkar <aboodsukkar24@live.com>
2025-10-08 21:04:08 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -14,24 +14,24 @@ fi
 echo "Setting up KernelSU-Next version: $KSU_NEXT_REF"
 bash KernelSU-Next/kernel/setup.sh "$KSU_NEXT_REF"
 
-cp KernelSU-Next/kernel/kernelsu.config arch/arm64/configs/vendor/kernelsu.config
-
 echo "CONFIG_KSU_NEXT=y" >> arch/arm64/configs/vendor/xiaomi/${DEVICE_CODENAME}_defconfig
 
 export ARCH=arm64
 export SUBARCH=arm64
 export KBUILD_COMPILER_STRING=$(clang --version | head -n1)
 export CCACHE_EXEC=$(which ccache)
-export KBUILD_BUILD_HOST="Caelum-Github-actions"
+export KBUILD_BUILD_HOST="Github-actions"
 export LLVM_IAS=1
 export CONFIG_BUILD_ARM64_APPENDED_DTB_IMAGE=y
 
-make O=out ARCH=arm64 vendor/xiaomi/${DEVICE_CODENAME}_defconfig vendor/kernelsu.config
+echo "CONFIG_BUILD_ARM64_APPENDED_DTB_IMAGE=y" >> arch/arm64/configs/vendor/xiaomi/miatoll_defconfig
+make O=out ARCH=arm64 vendor/xiaomi/miatoll_defconfig vendor/kernelsu.config
 yes "" | make O=out ARCH=arm64 olddefconfig
 
 make -j$(nproc --all) O=out \
     ARCH=arm64 \
     CC="ccache clang" \
+    LD=ld.lld \
     AR=llvm-ar \
     NM=llvm-nm \
     LLVM_IAS=1 \
@@ -40,4 +40,5 @@ make -j$(nproc --all) O=out \
     OBJDUMP=llvm-objdump \
     CLANG_TRIPLE=aarch64-linux-gnu- \
     CROSS_COMPILE=aarch64-linux-android- \
-    CROSS_COMPILE_ARM32=arm-linux-androideabi-
+    CROSS_COMPILE_ARM32=arm-linux-androideabi- \
+    CROSS_COMPILE_COMPAT=arm-linux-androidkernel-
